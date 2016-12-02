72b225ab967c53c986838b98c30b1aeea675998e
ThomasPeterson\Nick <ngcruess@gmail.com>
2016-12-01 21:21:18 -0600

    dsafljhdsaf

--- Purchase.cs
+++ Purchase.cs
@@ -11,7 +11,6 @@ namespace LonghornMusic.Models
     public class Purchase
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Display(Name="Purchase ID")]
         public  Int32 PurchaseId { get; set; }
         
